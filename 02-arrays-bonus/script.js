14792105985d5e4a4127166610990987c1cfc24b
EstebanCaponio <estebancaponio@gmail.com>
2025-02-27 11:45:37 +0100

    fix: fixed index to x.lenght-1

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -56,7 +56,7 @@ let teachersString = '';
 
 for (i = 0; i < teachers.length; i++) {
   teachersString += teachers[i];
-  if (i != 6) {
+  if (i != teachers.length - 1) {
     teachersString += ', ';
   } else
     teachersString += '.';
